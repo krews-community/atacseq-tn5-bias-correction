ad4fe68719b31d0cd827789376b0be46022cf925
hpratt <hpratt111@live.com>
2020-09-04 18:31:07 -0400

    check correct path for RGTDATA

--- src/main/__main__.py
+++ src/main/__main__.py
@@ -48,13 +48,14 @@ def main():
 
     cArgs = args()
 
-    if not os.path.exists("/rgtdata/{assembly}/genome_{assembly}.fa".format(assembly = cArgs.assembly)):
+    root = os.environ["RGTDATA"] if "RGTDATA" in os.environ else "/rgtdata"
+    if not os.path.exists(root + "/{assembly}/genome_{assembly}.fa".format(assembly = cArgs.assembly)):
         print(
             "WARNING: genomic data is not present for {assembly}. We will attempt to download it.".format(assembly = cArgs.assembly),
             file = sys.stderr
         )
         print(
-            "If you are running many jobs, they might run faster if you mount the appropriate data at /rgtdata/{assembly}.".format(assembly = cArgs.assembly),
+            "If you are running many jobs, they might run faster if you mount the appropriate data at {root}/{assembly}.".format(root = root, assembly = cArgs.assembly),
             file = sys.stderr
         )
         result = os.system("python3 /reg-gen/data/setupGenomicData.py --{assembly}".format(assembly = cArgs.assembly))
